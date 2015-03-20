2ca28baeab07fb26ceb19b5358fa6e8569637f75
Tabor Kvasnicka <kamakwazee@gmail.com>
2015-03-20 16:46:01 -0500

    Added some extraction code; however, there was an unforeseen logic error. I did not calculate for overlapping between different characters being offset in the y's clearly but the x's overlap. Same for the y's. Perhaps running tests the way they are, but also running them the opposite way may eliminate these issues.
    Signed-off-by: Tabor Kvasnicka <kamakwazee@gmail.com>

--- HandwritingRecognition/src/recognize.py
+++ HandwritingRecognition/src/recognize.py
@@ -44,8 +44,76 @@ class Main():
                 y += 1
             x += 1
             blk.append(blky)
-        return blk
-
+        return blk 
+    
+    #Extraction *logic error
+    def noMoreBlack(self,blk,x,y,mode):
+        print "x = " + str(x)
+        print "y = " + str(y)
+        if mode == 0:
+            print "Y's"
+            while y < 720:
+                if blk[x][y] == 1:
+                    return False
+                y += 1
+            return True
+        else:
+            print "X's"
+            while x < 1080:
+                if blk[x][y] == 1:
+                    return False
+                x += 1
+            return True
+        return True
+    
+    def extractCharacters(self, blk):
+        left = []
+        right = []
+        top = []
+        bottom = []
+        x = 0
+        inBlack = False
+        while x < 1080:
+            y = 0
+            while y < 720:
+                if blk[x][y] == 1:
+                    if inBlack == False:
+                        left.append(x)
+                        y = 720
+                        inBlack = True
+                else:
+                    if inBlack:
+                        if self.noMoreBlack(blk,x,y,0):
+                            right.append(x-1)
+                            inBlack = False
+                        y = 720
+                y += 1
+            x += 1
+        y = 0
+        inBlack = False
+        while y < 720:
+            x = 0
+            while x < 1080:
+                if blk[x][y] == 1:
+                    if inBlack == False:  
+                        top.append(y)
+                        x = 1080
+                        inBlack = True
+                else:
+                    if inBlack:
+                        if self.noMoreBlack(blk,x,y,1):
+                            bottom.append(y-1)
+                            inBlack = False
+                        x = 1080
+                x += 1
+            y += 1
+        print left
+        print right
+        print top
+        print bottom
+        return "Finished"            
+    #End extraction          
+          
     def plotPixels(self, pos):
         pygame.draw.rect(self.screen, (0,0,0), Rect((pos[0]-2,pos[1]-2),(4,4)))
 
@@ -58,7 +126,7 @@ class Main():
                 self.plotPixels(event.pos)
             elif event.type == KEYDOWN:
                 if event.key == K_RETURN:
-                    self.outputToFile(self.findBlack())
+                    self.outputToFile(self.extractCharacters(self.findBlack()))
                 elif event.key == K_SPACE:
                     self.screen.fill((255,255,255))
 
